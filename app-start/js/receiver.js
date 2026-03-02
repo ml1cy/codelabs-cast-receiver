8f7d0f2de5368ae1f8920490a02359d49f9a5983
Ijaz Ali <ijaz@1cy.tech>
2026-03-02 14:20:54 -0500

    dsfsd

--- app-start/js/receiver.js
+++ app-start/js/receiver.js
@@ -1,4 +1,11 @@
-const context = cast.framework.CastReceiverContext.getInstance();
-const playerManager = context.getPlayerManager();
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 
+const context = cast.framework.CastReceiverContext.getInstance();
+
+context.addEventListener(cast.framework.system.EventType.READY, () => {
+  if (!castDebugLogger.debugOverlayElement_) {
+      // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+      castDebugLogger.setEnabled(true);
+  }
+});
 context.start();
